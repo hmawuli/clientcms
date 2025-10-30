158116a083c2571d0f52e0d10412d67e88487417
Hormeku Mawuli <hormekumawuli93@gmail.com>
2025-10-30 05:56:01 +0000

    reworked on the about page with the about controller

---
+++ resources/views/frontend/about.blade.php
@@ -0,0 +1,8 @@
+@extends('frontend.layouts.app')
+
+@section('title', 'About Us')
+
+@section('content')
+<h2 class="text-3xl font-bold mb-4">About Us</h2>
+<p class="text-gray-700">Information about your company goes here.</p>
+@endsection
